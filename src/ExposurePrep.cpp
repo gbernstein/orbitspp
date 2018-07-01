14bea06976d780421c2f385b863e3fa8a09caccf
Gary Bernstein <garyb@physics.upenn.edu>
2018-06-30 21:31:45 -0400

    ExposurePrep works

--- src/ExposurePrep.cpp
+++ src/ExposurePrep.cpp
@@ -94,7 +94,75 @@ main(int argc, char *argv[]) {
     quit(m);
   }
 
+  try {
+    // Create new information about exposures
+    orbits::Ephemeris ephem;
+    std::vector<std::vector<double>> obspos; // Observatory ICRS coords
+    std::vector<double> ecl_lat;
+    std::vector<double> ecl_lon;
+    std::vector<double> obs_ecl_lon;  // Ecliptic longitude of observatory
+
+    // Covariance to insert where warning is set.
+    const std::vector<double> DEFAULT_COV = {pow(15.,2.),pow(15.,2.),0.};
+
+    double mjd, ra, dec;
+    for (int i=0; i<exposureTable.nrows(); i++) {
+      double mjd;
+      exposureTable.readCell(mjd, "mjd_mid",i);
+      // Get observatory position
+      astrometry::CartesianICRS observatory = ephem.observatory(807, ephem.mjd2tdb(mjd));
+      std::vector<double> v3 = {observatory[0],
+				observatory[1],
+				observatory[2]};
+      obspos.push_back(v3);
+
+      {
+	// Get ecliptic longitude of observatory
+	astrometry::CartesianEcliptic cc(observatory);
+	astrometry::SphericalEcliptic ecl(cc);
+	double lon,lat;
+	ecl.getLonLat(lon, lat);
+	obs_ecl_lon.push_back(lon/DEGREE);
+      }
+	
+      {
+	// Get ecliptic coordinates of pointing
+	exposureTable.readCell(ra,"ra",i);
+	exposureTable.readCell(dec,"dec",i);
+	astrometry::SphericalICRS icrs(ra*DEGREE, dec*DEGREE);
+	astrometry::SphericalEcliptic ecl(icrs);
+	double lon,lat;
+	ecl.getLonLat(lon, lat);
+	ecl_lon.push_back(lon/DEGREE);
+	ecl_lat.push_back(lat/DEGREE);
+      }
+
+      // Replace cov with default if warned
+      bool warn;
+      exposureTable.readCell(warn,"covwarn",i);
+      if (warn)
+	exposureTable.writeCell(DEFAULT_COV, "cov", i);
+      
+    } 
+    exposureTable.addColumn(obspos,"observatory",3);
+    exposureTable.addColumn(ecl_lon,"ecl_lon");
+    exposureTable.addColumn(ecl_lat,"ecl_lat");
+    exposureTable.addColumn(obs_ecl_lon,"obs_ecl_lon");
+  } catch (std::runtime_error& m) {
+    quit(m);
+  }
+
+  
+  // Write the exposure table to disk
   string outFile = argv[3];
+  try {
+    FITS::FitsTable out(outFile,FITS::Create+FITS::OverwriteFile);
+    out.copy(exposureTable);
+  } catch (FITS::FITSError& m) {
+    cerr << "Error writing output file " << outFile << endl;
+    quit(m);
+  }
+  
   exit(0);
 }
 
