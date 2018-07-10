3f9e78eff9b68a781656236f582b271ce7971540
Gary Bernstein <garyb@physics.upenn.edu>
2018-07-09 21:19:13 -0400

    More on Nodes

--- include/Exposures.h
+++ include/Exposures.h
@@ -15,6 +15,7 @@ namespace orbits {
     int expnum;
     double mjd;
     double tdb;
+    double tobs;  // Time relative to Frame tdb0
     astrometry::Vector3 earth;  // Observatory position in Frame
     Point axis;   // Optical axis coords, in Frame gnomonic, degrees
 
@@ -27,7 +28,9 @@ namespace orbits {
     // Node of a k-d tree of Exposures over time and position
   public:
     typedef vector<const Exposure*>::iterator dataIter;
-    Node( dataIter _begin, dataIter _end);
+    Node( dataIter _begin, dataIter _end,
+	  const Ephemeris& ephem,
+	  const Frame& frame);
     ~Node() {
       if (left) delete left;
       if (right) delete right;
@@ -35,30 +38,43 @@ namespace orbits {
     
     enum Coordinate {TIME, X, Y};  // The three axes of the tree space
     
-    Point lower;  // Corners of the node
-    Point upper;
-    double tStart;  // Time (relative to reference frame tdb0) of
-    double tStop;   // first and last exposure in set
-
-    void split();
-    // Split all the way down to leaves, starting with 
-
     list<const Exposure*> find(const Fitter& path) const;
     // Return a list of exposures that are close to the path.
 
     static void setSpeed(double speed_) {speed=speed_;}
     static void setFieldRadius(double radius_) {fieldRadius = radius_;}
+    static void setObservatory(int obscode_) {obscode=obscode_;}
+    static Node* buildTree(dataIter begin_, dataIter end_,
+			   const Ephemeris& ephem,
+			   const Frame& frame);
+    // Builds the whole tree out of time-ordered data.
+    // Be sure to set speed and radius first.  Returns root node pointer.
+    // Needs 
     
   private:
     dataIter begin;  // Range of data associated with this node
     dataIter end;
     Node* left;	// Children (nullptrs if leaf node)
     Node* right;
+    Point lower;  // Corners of the node
+    Point upper;
+    double tStart;  // Time (relative to reference frame tdb0) of
+    double tStop;   // first and last exposure in set
+
     Coordinate splitOn;  // Which coordinate does the split happen on?
     double splitValue;   // Value for split
 
     static double speed;  // Typical object speed, used to choose time vs distance split
     static double fieldRadius; // Size of exposure FOV.  Also sets the scale for radius.
+    static int obscode;  // Observatory code for earth positions.
+    
+    DVector tobs; // Time (relative to tdb0) of begin, midpoint, and end of Node's exposures.
+    DMatrix earth; // Position of observatory at begin, mid, and end times.
+    
+    void split(const Ephemeris& ephem,
+	       const Frame& frame);
+    // Split all the way down to leaves
+
   };
   
   extern
--- include/PlaneGeometry.h
+++ include/PlaneGeometry.h
@@ -19,6 +19,8 @@ namespace orbits {
     Point(double x=0., double y=0.) {
       (*this)[0] = x; (*this)[1] = y;
     }
+    Point(const Vector2& rhs): Vector2(rhs) {}
+    
     double distanceSq(const Point& rhs) const {
       return (*this - rhs).squaredNorm();
     }
@@ -29,12 +31,12 @@ namespace orbits {
     }
   };
 
-  
   class Ray {
     // Class describing a directed line that goes from p1 to p2
   public:
     Ray(const Point& p1_, const Point& p2_);
     bool onRight(const Point& p) const;  // Is the point to the right of the right?
+    BVector onRight(const DMatrix& pts) const;  // same for Nx2 array of points
     double distance(const Point& p) const; // Perp distance to point from line (signed???)
     DVector distance(const DMatrix& p) const; // Distance to a list of points (Nx2 matrix)
     
@@ -43,6 +45,18 @@ namespace orbits {
     Point p2;
   };
 
+  class ConvexPolygon {
+    // Convex polygon defined by clockwise list of vertices.  There is no automatic check
+    // for convexity or clockwise.
+  public:
+    ConvexPolygon(const vector<Point>& vertices);
+    bool isConvex() const; // Force check
+    double area() const;
+    // Check whether one or many points are interior to polygon
+    bool inside(const Point& p) const;
+    BVector inside(const DMatrix& pts) const;
+  };
+    
   class Circle {
   public:
     Circle(const Point& ctr_, double radius_): ctr(ctr_), radius(radius_), rsq(radius_*radius_) {}
@@ -69,17 +83,24 @@ namespace orbits {
     // Return true if circle boundaries cross.
     // In which case determine intersection points
     
+    bool intersects(const ConvexPolygon& rhs) const;
+    // True if finite area in common.
+    double commonArea(const ConvexPolygon& rhs) const;
+    // Return area of intersection with polygon.
+
   private:
     double rsq;
   };
 
+  class AffineTransformation; // Forward declaration
+  
   class Ellipse {
     // Class representing elliptical region
-    Ellipse(const Point& ctr_, const Matrix22& cov_): ctr(ctr_), cov(cov_) {}
+    Ellipse(const Point& ctr_, const Matrix22& cov_): ctr(ctr_), cov(cov_) {setup();}
     Point ctr;
     Matrix22 cov;
     
-    double area() const;
+    double area() const {return sqrt(detCov);}
 
     bool inside(const Point& rhs) const; // Is point interior to ellipse?
     BVector inside (const DMatrix& rhs) const; // Nx2 array of points
@@ -87,12 +108,48 @@ namespace orbits {
     double chisq(const Point& rhs) const; // Return quadratic form
     DVector chisq(const DMatrix& rhs) const; // Return quadratic form for Nx2 array
 
-    int intersect(const Circle& rhs) const;
-    int intersect(const Ellipse& rhs) const;
+    int roughIntersects(const Circle& rhs) const;
+    int roughIntersects(const Ellipse& rhs) const;
     // Return -1, 0, 1 as circle definitely doesn't, might, or definitely does
     // intersect the circle, based on major/minor axes.
 
     void getSemiMajorMinor(double& a, double& b) const ;
+    AffineTransformation getCircularizer() const;
+    // Returns a transformation that renders Ellipse into unit circle.
+
+    bool intersect(const ConvexPolygon& rhs) const;
+    // True if finite area in common.
+    double commonArea(const ConvexPolygon& rhs) const;
+    // Return area of intersection with polygon.
+
+  private:
+    void setup(); // Calculate various things...
+    double detCov; // Determinant of covariance
+    Matrix22 invCov;
+    // ?? Eigenvalues etc
+    
+  };
+
+  class AffineTransformation {
+    // Map of the form (x,y) -> M * (x-x0, y-y0)
+  public:
+    AffineTransformation(const Point& xy0_, const Matrix22& m_): xy0(xy0_), m(m_) {}
+    // Operate on single or  set of points as Nx2 matrix
+    Point operator()(const Point& p) const {
+      Vector2 out = m * (p-xy0);
+      return out;
+    }
+    DMatrix operator()(const DMatrix& pts) const {
+      return (pts.rowwise()-xy0.transpose()) * m.transpose();
+    }
+
+    // Operate on other shapes
+    Ray operator()(const Ray& r) const;
+    Ellipse operator()(const Circle& c) const;
+    Ellipse operator()(const Ellipse& c) const;
+  private:
+    Point xy0;
+    Matrix22 m;
   };
     
 } // end namespace
