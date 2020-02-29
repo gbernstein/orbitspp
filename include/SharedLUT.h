da2c52bd458b060b785399f396fcd8a2a56234e0
Gary Bernstein <garyb@physics.upenn.edu>
2020-02-29 03:49:30 -0500

    SharedLUT now grows both directions and Trajectory uses it successfully.

--- include/SharedLUT.h
+++ include/SharedLUT.h
@@ -5,23 +5,18 @@
 
 template <class T, class Allocator=std::allocator<T>>
 class SharedLUT {
-  /* A lookup-table indexed by integers 0,1,...,N which is thread-safe
+  /* A lookup-table indexed by integers iBegin,...,iEnd which is thread-safe
    * and retains lock-free, O(1) access on reads.
-   * A C++ mutex lock is used for the appending of new entries to the table.
-   * It is assumed that appending is rare, and deleting entries is not allowed.
+   * A C++ mutex lock is used for the appending or prepending of new entries to the table.
+   * It is assumed that pre/appending is rare, and deleting entries is not allowed.
    * No iterators will be issued.
    */
  public:
-  typedef std::shared_ptr<std::vector<T,Allocator>> Sptr;
-  SharedLUT() {
-    sptr.reset(new std::vector<T,Allocator>);
-  }
+  SharedLUT() {}
+
   template <typename Iter>
-  SharedLUT(Iter it, Iter end) {
-    sptr.reset(new std::vector<T,Allocator>() );
-    sptr->reserve(end-it);
-    for ( ; it!=end; ++it)
-      sptr->push_back(*it);
+  SharedLUT(int iStart, Iter it, Iter end) {
+    extend(iStart,it,end);
   }
 
   // Read element
@@ -29,81 +24,160 @@ class SharedLUT {
     // Get a shared pointer to the current vector of data,
     // so it will persist until the operation is done, even
     // if another thread changes the current vector's location.
-    Sptr tmp_ptr(sptr);
+    LUTptr tmp_ptr(lutptr);
+    return (*tmp_ptr)[i];
+  }
+  // Range-checked read
+  T at(size_t i) const {
+    LUTptr tmp_ptr(lutptr);
+    if (empty() || i<tmp_ptr->iStart() || i>=tmp_ptr->iEnd())
+      throw std::out_of_range("SharedLUT index out of bounds");
     return (*tmp_ptr)[i];
   }
 
-  // Current element count
-  const size_t size() const {return sptr->size();}
+  // Current element counts
+  bool empty() const {return !lutptr;}
+  const size_t size() const {return lutptr ? lutptr->size() : 0;}
+  const int iStart() const {return lutptr->iStart();}
+  const int iEnd() const {return lutptr->iEnd();}
 
-  // Write one or a sequence of elements to the table.
+  // Extend LUT by one element or a sequence.
   // argument i is the index for first write.
-  // It is an error to attempt to write that starts more than one
-  // past the end of the table, and any writes
+  // It is an error to attempt to write that would leave
+  // a gap between extension and existing data.
+  // Any writes
   // to table elements that already exist are IGNORED.
-  void append(size_t i, const T& t) {
-    if (i<sptr->size()) {
-      // We already go this far
-      return;
-    } else if (i>sptr->size()+1) {
-      // 
-      throw std::runtime_error("ERROR: appending past the end of a SharedLUT");
-    }
-    // Acquire the lock
-    std::lock_guard<std::mutex> g(writeLock);
-    if (sptr->capacity() - sptr->size() > 0) {
-      sptr->push_back(t);
-    } else {
-      // Make a fresh vector by hand
-      auto vnew = new std::vector<T,Allocator>();
-      vnew->resize(sptr->size() + 1);
-      std::copy(sptr->begin(), sptr->end(), vnew->begin());
-      // Add new element
-      (*vnew)[sptr->size()] = t;
-      // atomically point sptr to the new vector.
-      // The smart pointer will destroy the old one if
-      // no one else is using it.
-      sptr.reset(vnew);
+  void extend(int i, const T& t) {
+    if (empty() || i==iStart()-1 || i==iEnd()) {
+      // We should insert the item
+      // Acquire the lock
+      std::lock_guard<std::mutex> g(writeLock);
+      if (empty()) {
+	auto newlut = new LUT();
+	newlut->i0 = i;
+	newlut->data.push_back(t);
+	// Install the LUT atomically
+	lutptr.reset(newlut);
+      } else if (i==iStart()-1) {
+	// prepend - make new copy as all iterators become invalid
+	auto newlut = new LUT();
+	newlut->data.resize(size()+1);
+	newlut->i0 = lutptr->i0-1;
+	// Install new point first
+	(newlut->data)[0] = t;
+	// Then copy remainder over
+	std::copy(lutptr->data.begin(), lutptr->data.end(), newlut->data.begin()+1);
+	// And redirect lutptr to new one.
+	lutptr.reset(newlut);
+      } else if (i==iEnd()) {
+	if (lutptr->data.capacity() > lutptr->size()) {
+	  // Can safely just append
+	  lutptr->data.push_back(t);
+	} else {
+	  // Need to expand capacity, so get new LUT object
+	  auto newlut = new LUT();
+	  newlut->data.resize(size()+1);
+	  newlut->i0 = lutptr->i0;
+	  // Copy data
+	  std::copy(lutptr->data.begin(), lutptr->data.end(), newlut->data.begin());
+	  // Install new point 
+	  (newlut->data)[iEnd()] = t;
+	  // And redirect lutptr to new one.
+	  lutptr.reset(newlut);
+	}
+      }
+    } else if (i>iEnd() || i<iStart()-1) {
+      throw std::runtime_error("ERROR: non-contiguous SharedLUT::extend()");
     }
   }
+  
   template <typename Iter>
-  void append(size_t i, Iter it, Iter end) {
-    size_t nAdd = end-it;
-    size_t s = sptr->size();
-    if (i+nAdd <= s) {
-      // Table already goes this far, nothing to do.
+  void extend(int i, Iter it, Iter end) {
+    int nAdd = end-it;
+    if (empty()) {
+      // Create and install new LUT
+      std::lock_guard<std::mutex> g(writeLock);
+      auto newlut =  new LUT();
+      newlut->i0 = i;
+      newlut->data.resize(nAdd);
+      std::copy(it,end,newlut->data.begin());
+      lutptr.reset(newlut);
       return;
-    } else if (i>s+1) {
-      // 
-      throw std::runtime_error("ERROR: appending past the end of a SharedLUT");
+    } else if (i>iEnd() || i+nAdd<iStart()) {
+      // Leaving a gap - not allowed
+      throw std::runtime_error("ERROR: non-contiguous SharedLUT::extend()");
+    } else if (i>=iStart() && i+nAdd<=iEnd()) {
+      // "extension" already contained in LUT.  Nothing to do.
+      return;
     }
-    // Acquire the lock
+
+    // Need to append and/or prepend.  Lock array 
     std::lock_guard<std::mutex> g(writeLock);
-    s = sptr->size(); // Re-read size while locked
-    // Advance input iterator for already-known elements
-    for (int j=i; j<s && it!=end; ++j) {
-      ++it;
-      --nAdd;
+    int newStart = std::min(i,iStart());
+    int newEnd = std::max(i+nAdd, iEnd());
+    size_t newsize = newEnd - newStart;
+    if (newStart<iStart()) {
+      // Prepending to do.  Make new LUT
+      auto newlut =  new LUT();
+      newlut->i0 = newStart;
+      newlut->data.resize(newsize);
+      // Copy in new data until we reach the old stuff
+      auto newit = newlut->data.begin();
+      for (int j=i; j<iStart(); ++it, ++j, ++newit) {
+	*newit = *it;
+      }
+      // Then copy the old data
+      newit = std::copy(lutptr->data.begin(), lutptr->data.end(), newit);
+      it += size();
+      
+      // Also copy appended elements if any
+      if (it < end)
+	std::copy(it,end,newit);
+
+      // Install new table
+      lutptr.reset(newlut);
+      return;
     }
-    if (sptr->capacity() - sptr->size() >= nAdd) {
-      for (; it!=end; ++it)
-	sptr->push_back(*it);
-    } else {
-      // Make a fresh vector by hand
-      auto vnew = new std::vector<T,Allocator>();
-      vnew->resize(s + nAdd);
-      std::copy(sptr->begin(), sptr->end(), vnew->begin());
-      // Add new elements (potentially rewriting some old ones)
-      std::copy(it, end, vnew->begin()+s);
-      // atomically point sptr to the new vector.
-      // The smart pointer will destroy the old one if
-      // no one else is using it.
-      sptr.reset(vnew);
+
+    // Need to append?
+    if (newEnd > iEnd()) {
+      if (lutptr->data.capacity() >= newsize) {
+	// Just append to existing array, will not invalidate iterators
+	size_t oldsize = lutptr->data.size();
+	lutptr->data.resize(newsize);
+	std::copy( it + (lutptr->i0 + oldsize -i), end, lutptr->data.begin()+oldsize);
+      } else {
+	// Need to expand array, make & install new LUT
+	auto newlut =  new LUT();
+	newlut->i0 = newStart;
+	newlut->data.resize(newsize);
+	auto newit = newlut->data.begin();
+	// Copy the old data
+	newit = std::copy(lutptr->data.begin(), lutptr->data.end(), newit);
+	std::copy( it + (iEnd()-i), end, newit);
+
+	// Install new table
+	lutptr.reset(newlut);
+      }
     }
+    return;
   }
 
 private:
-  Sptr sptr;
+ private:
+  // Define private structure holding the data and the starting index
+  struct LUT {
+  public:
+    int i0;  // Index that accesses first element of vector
+    std::vector<T,Allocator> data;
+    size_t size() const {return data.size();}
+    int iStart() const {return i0;}
+    int iEnd() const {return i0 + size();}
+    T operator[](int i) const {return data[i-i0];} // No range check
+  };
+  // The table structure itself: hold via shared pointer
+  typedef std::shared_ptr<LUT> LUTptr;
+  LUTptr lutptr;  
   // The write lock for this realization
   std::mutex writeLock;
 };
--- tests/testSharedLUT.cpp
+++ tests/testSharedLUT.cpp
@@ -10,19 +10,19 @@ int main(int argc, char *argv[]) {
 
   int exitcode=0;
   SharedLUT<int> lut;
-  
+  /**/cerr << "Entering parallel" << endl;
 #pragma omp parallel for
   for (int i=0; i<atoi(argv[1]); i++) {
     // Each thread will fill the table 10 entries past its
     // number if the table does not already reach its number
-    if (lut.size() <=i) {
+    if (lut.empty() || lut.iEnd() <=i) {
       vector<int> v;
-      int s=lut.size();
+      int s=lut.empty() ? 0 : lut.iEnd();
       for (int j=s; j<=i+10; j++)
 	v.push_back(2*j);
 #pragma omp critical(io)
-      cerr << "Appending " << s << " to " << i+10 << endl;
-      lut.append(s, v.begin(), v.end());
+      cerr << "Extending " << s << " to " << i+10 << endl;
+      lut.extend(s, v.begin(), v.end());
     }
     // Every loop step reads its value
     if (lut[i]!=2*i) {
@@ -31,6 +31,27 @@ int main(int argc, char *argv[]) {
     }
   }
 
+  /**/cerr << "Entering negative parallel" << endl;
+#pragma omp parallel for
+  for (int i=0; i>-atoi(argv[1]); i--) {
+    // Each thread will fill the table 10 entries past its
+    // number if the table does not already reach its number
+    if (lut.iStart() > i) {
+      vector<int> v;
+      int s=lut.iStart();
+      for (int j=i-10; j<s; j++)
+	v.push_back(2*j);
+#pragma omp critical(io)
+      cerr << "Extending " << s << " to " << i-10 << endl;
+      lut.extend(i-10, v.begin(), v.end());
+    }
+    // Every loop step reads its value
+    if (lut[i]!=2*i) {
+      cerr << "Error in value at i=" << i << " " << lut[i] << endl;
+      exitcode=1;
+    }
+  }
+  
   exit(exitcode);
 }
 
